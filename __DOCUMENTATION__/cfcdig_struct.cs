91e6fa5d2ce494bab5fc78a3352cdeb331086d0e
matheusvtz <matheusvettorazzi@gmail.com>
2026-04-09 12:18:21 -0300

    Update cfcdig_struct.cs

--- __DOCUMENTATION__/cfcdig_struct.cs
+++ __DOCUMENTATION__/cfcdig_struct.cs
@@ -1,8 +1,8 @@
 struct FileRecord{
-	uint offset; // Divided by 0x800
-	uint packedSize;
-	ushort sectionCount;
+	int offset; // Divided by 0x800
+	int packedSize;
+	short sectionCount;
 	bool isCompressed;
 	byte padding;
-	uint orgSize;
+	int diskSize;
 }
